c6631a4730a1cf28cd9d45d7994d25a4024b7361
Arkadiusz Plichta <el.pikel@gmail.com>
2016-12-22 17:54:30 +0100

    Recursion

--- lib/calc.ex
+++ lib/calc.ex
@@ -2,4 +2,8 @@ defmodule Calc do
   def sum(a \\ 3, b, c \\ 7) do
     a + b + c
   end
+
+  def gdc(m, n) when m == n, do: m
+  def gdc(m, n) when m > n, do: gdc(m-n, n)
+  def gdc(m, n), do: gdc(m, n-m)
 end
--- test/calc_test.exs
+++ test/calc_test.exs
@@ -13,4 +13,24 @@ defmodule CalcTest do
   test "sets default values for missing params" do
     assert 21 == Calc.sum(11)
   end
+
+  test "when m == n then greatest common divisor is m" do
+    assert 22 == Calc.gdc(22, 22)
+  end
+
+  test "greatest common divisor of 2 and 8 is 2" do
+    assert 2 == Calc.gdc(2,8)
+  end
+
+  test "greatest common divisor of 14 and 21 is 7" do
+    assert 7 == Calc.gdc(14, 21)
+  end
+
+  test "greatest common divisor of 125 and 46 is 1" do
+    assert 1 == Calc.gdc(125, 46)
+  end
+
+  test "greatest common divisor of 120 and 36 is 1" do
+    assert 12 == Calc.gdc(120, 36)
+  end
 end
