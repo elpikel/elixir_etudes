2c39421e95b64115cc83a8f1dc7c9a7dc6143210
Arkadiusz Plichta <Arkadiusz.Plichta@powel.com>
2016-12-30 10:54:40 +0100

    Tail Recursion with an Accumulator

--- lib/powers.ex
+++ lib/powers.ex
@@ -1,6 +1,8 @@
 defmodule Powers do
-  def rise(_x, 0), do: 1
-  def rise(x, 1), do: x
-  def rise(x, n) when n < 0, do: (1 / rise(x, -n))
-  def rise(x, n), do: x * rise(x, n - 1)
+  def rise(x, n), do: rise(x, n, 1)
+
+  def rise(_x, 0, _), do: 1
+  def rise(x, 1, acc), do: x * acc
+  def rise(x, n, acc) when n < 0, do: (1 / rise(x, -n, acc))
+  def rise(x, n, acc), do: rise(x, n - 1, acc * x)
 end
