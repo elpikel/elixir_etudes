a368380b851f5605973b2739503b46a0581b68e1
Arkadiusz Plichta <el.pikel@gmail.com>
2017-01-01 17:46:46 +0100

    Accumulating the Sum of a List

--- lib/dates.ex
+++ lib/dates.ex
@@ -4,4 +4,29 @@ defmodule Dates do
     |> String.split("-")
     |> Enum.map(fn(item) -> String.to_integer(item) end)
   end
+
+  def julian(date) do
+    [y, m, d] = get_parts(date)
+
+    days_in_feb = cond do
+      is_leap_year(y) -> 29
+      true -> 28
+    end
+
+    days_per_month = [31, days_in_feb, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    month_total(m, days_per_month, 0) + d
+  end
+
+  defp is_leap_year(year) do
+    (rem(year, 4) == 0 and rem(year, 100) != 0) or (rem(year, 400) == 0)
+  end
+
+  defp month_total(1, _days_per_month, total) do
+    total
+  end
+
+  defp month_total(m, [this_month|other_months], total) do
+    month_total(m - 1, other_months, total + this_month)
+  end
 end
--- test/dates_test.exs
+++ test/dates_test.exs
@@ -4,4 +4,9 @@ defmodule DatesTest do
   test "should split date by -" do
     assert Dates.get_parts("2013-06-15") == [2013, 6, 15]
   end
+
+  test "returns the Julian date: the day of the year" do
+    assert Dates.julian("2013-12-31") == 365
+    assert Dates.julian("2012-12-31") == 366
+  end
 end
